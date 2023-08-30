c44797d4778b220c93f64e8618ff64c25926d881
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-08-29 21:40:09 -0700

    added GET and POST routes

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+
 const PORT = process.env.PORT || 3000
 const db = require('./db/db.json')
 const util = require("util")
@@ -11,14 +13,32 @@ const app = express ();
 
 app.use(express.static('public'))
 app.use(express.json())
+//  this is getting notes from the JSON file
+app.get('/api/notes', function (req, res){
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let dbData = JSON.parse(data)
 
-app.get('/', function (req, res){
-    res.sendStatus(500)
+        res.json(dbData)
+    })
+})
+
+app.post('/api/notes', (req, res) =>{
+    const newNote = req.body
+
+    db.push(newNote)
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+
+    res.json(db)
+    
 })
 
 app.get('/alien', function (req, res){
     res.send('Welcome Back Alien')
 })
-app.listen(9000, function (req, res){
-    console.log('running')
+
+// this is starting the server
+app.listen(PORT, function (){
+    console.log(`Server is running on port ${PORT}`)
 });
