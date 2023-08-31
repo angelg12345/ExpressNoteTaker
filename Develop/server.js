60bb2c6d7fb52cf25c6b316230ed9efaa554aad5
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-08-30 23:27:33 -0700

    cleaned code and added error code

--- Develop/server.js
+++ Develop/server.js
@@ -2,46 +2,80 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
-
-const PORT = process.env.PORT || 3000
-const db = require('./db/db.json')
-
+const {v4: uuidv4 } = require('uuid')
 
 
 const app = express ();
 
+const PORT = process.env.PORT || 3000;
+const dbFilePath = path.join(__dirname, "db", "db.json");
+
+
 app.use(express.static('public'))
 app.use(express.json())
+
+
+
+
+
 //  this is getting notes from the JSON file
 app.get('/api/notes', function (req, res){
-    fs.readFile('./db/db.json', (err, data) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
         if (err) throw err;
         let dbData = JSON.parse(data)
 
         res.json(dbData)
-    })
-})
+    });
+});
 
 app.post('/api/notes', (req, res) =>{
     const newNote = req.body
 
-    db.push(newNote)
+    newNote.id = uuidv4()
 
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+   
 
-    res.json(db)
+    const db = JSON.parse(data);
+    db.push(newNote);
 
+    fs.writeFile(dbFilePath, JSON.stringify(db), (err) => {
+        if (err) {
+            console.error(err);
+        }
+        res.json(db)
+    })
+
+   
+})
 })
 
 
 app.delete('/api/notes/:id', (req, res) => {
-    const noteIDtoDelete = req.params.id
-    const newDb = db.filter((note) => 
-    note.id !== noteIDtoDelete)
-    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
-    res.json(newDb);
-})
+    const noteIDtoDelete = req.params.id;
+
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        const db = JSON.parse(data);
+        const newDb = db.filter((note) => note.id !== noteIDtoDelete);
+
+        fs.writeFile(dbFilePath, JSON.stringify(newDb), (err) => {
+            if (err) {
+                console.error(err);
+            }
+            res.json(newDb);
+        });
+    });
+});
+
+
+
+
 
 app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
